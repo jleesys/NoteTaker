4a1ea1c8024b58e5004d6620b1eb656a8517f579
jleesys949 <jlee.post24@gmail.com>
2022-11-18 14:23:12 -0800

    Add state hooks for inputs

--- notetaker/src/App.js
+++ notetaker/src/App.js
@@ -1,19 +1,43 @@
-import note from "./components/note"
+import note from "./components/note";
+import { useState } from "react";
+
 
 const testNotes = [
-  "Test Note 1",
-  "Test Note 2",
-  "Test Note 3",
-  "Test Note 4"
+  {content:"Test Note 1"},
+  {content:"Test Note 2"},
+  {content:"Test Note 3"},
+  {content:"Test Note 4"}
 ]
+
 function App() {
+  const [notes, setNotes] = useState(testNotes);
+  const [noteText, setNoteText] = useState("");
+  const [finalNote, setFinalNote] = useState("");
+  window.notes = notes;
+
+  const handleNoteInput = (event) => {
+    setNoteText(event.target.value);
+  } 
+  const handleNoteSubmission = (event) => {
+    event.preventDefault();
+    const noteToAdd = {
+      content: noteText
+    } 
+    setNotes(notes.concat(noteToAdd));
+  }
+
   return (
     <div>
       <h1>Note Taker</h1>
       <h2>Add Notes</h2>
-      <form></form>
+      <form onSubmit={handleNoteSubmission}>
+        <input onChange={handleNoteInput} placeholder="Enter note here"></input>
+        <div>
+          <button type="submit">Submit Note</button>
+        </div>
+      </form>
       <h2>Notes</h2>
-      {testNotes.map(note => <div>{note}</div>)}
+      {notes.map(note => <div>{note.content}</div>)}
     </div>
   );
 }
