e1ca7b6e3d6f5eafc7c61a1b7e416f162dfbefd1
jleesys949 <jlee.post24@gmail.com>
2022-11-18 19:05:18 -0800

    Install dotenv and port var

--- notetaker.backend/index.js
+++ notetaker.backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
+require('dotenv').config()
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT;
 
 app.listen(PORT, (err) => {
     if (!err) {
