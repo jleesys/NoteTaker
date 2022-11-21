9b793585526780a08c84d2f9a1c8febaf2a0086d
jleesys949 <jlee.post24@gmail.com>
2022-11-20 19:06:06 -0800

    New frontend build, update model

---
+++ notetaker.backend/models/note.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const url = process.env.MONGODB_URL;
+
+// create schema
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+    date: String
+})
+
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+// create model
+const Note = mongoose.model('Note', noteSchema);
+
+console.log(url);
+mongoose
+    .connect(url)
+    .then(result => {
+        console.log(`connecting to url ${url}`)
+        console.log(`connection success`)
+    })
+    .catch (err => {
+    console.log(`woops, connection failed : `, err)
+})
+
+module.exports = Note
