6aee3a07d82136344db10652eb00ce3530bae343
jleesys949 <jlee.post24@gmail.com>
2022-12-06 20:53:30 -0800

    Create test suite for users

--- notetaker.backend/tests/test_helper.js
+++ notetaker.backend/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Note = require('../models/note');
+const User = require('../models/user');
 
 const initialNotes = [
     {
@@ -34,4 +35,10 @@ const notesInDb = async () => {
     return notes.map(note => note.toJSON());
 };
 
-module.exports = { initialNotes, nonExistingId, notesInDb };
+const usersInDb = async () => {
+    const users = await User.find({});
+    return users.map(user => user.toJSON());
+    // return users.map(user => JSON.stringify(user));
+};
+
+module.exports = { initialNotes, nonExistingId, usersInDb, notesInDb };
